bb62fe907bd3033780fe08e24594e1f3f0ca4706
oschnack <56824586+oschnack@users.noreply.github.com>
2021-04-09 15:14:51 +0200

    Add files via upload

--- send.cpp
+++ send.cpp
@@ -8,7 +8,7 @@
 
 using namespace std;
 
-string sanitize(string message){
+string sanitize(string message){// Sanitize renvoie le message entré en full majuscule
   string res;
   int i = 0;
   vector<char> test;
@@ -24,13 +24,76 @@ string sanitize(string message){
     res += toupper(test.data()[i]);
     i++;
   }
-    return res;
-    
+    return res; 
 }
-// TODO implémenter les fonctions sanitize, caesar et send ici
+
+
+string caesar(string message, int sens, int decalage){//s'occupe de l'encrpytage du message
+  int trans;
+  string res;
+  switch (sens)
+  {
+  case 1:{//cas decalage des lettres vers la droite
+    for (char& c : message)
+  {
+    if (int(c) == 32)
+    {
+      res += ' ';
+      continue;
+    }else{
+      trans = int(c);
+      trans += decalage;
+      if (trans > 90)
+      {
+        trans -= 26;
+      }
+    }
+    res += char(trans);
+    
+  }
+    break;
+  }
+  case 0:{//cas decalage des lettres vers la gauche
+    for (char& c : message)
+  {
+    if (int(c) == 32)
+    {
+      res += ' ';
+      continue;
+    }else{
+      trans = int(c);
+      trans -= decalage;
+      if (trans < 65)
+      {
+        trans += 26;
+      }
+    }
+    res += char(trans);
+    
+  }
+    break;
+  }
+  default:{
+    break;
+  }
+  }
+  cout << "Envoie du message: " << res <<endl;
+  return res;
+}
+
+void send(string message, int sens, int decalage){
+  string filename = "test.txt";
+  ofstream file(filename, ios_base::app);
+if(file.is_open()) {
+  file << caesar(sanitize(message), sens, decalage) << endl;
+  file.close();
+}
+else cerr << "Impossible d’ouvrir le fichier " << filename << endl;
+}
 
 int main(int argc, char *argv[]) {
-    // Les arguments à fournir: G|D n fichier
+    string message;
+    int sens;
     if (argc < 4) {
         cout << "Utilisation: ./send G|D décalage fichier." << endl;
         return 1;
@@ -40,10 +103,14 @@ int main(int argc, char *argv[]) {
         return 1;
     }
     bool rightShift = strcmp(argv[1], "D") == 0;
-    // atoi: conversion d'un string vers un entier
     int shift = atoi(argv[2]);
     string filename = argv[3];
-    // TODO Votre programme commence ici
+    cout <<"test " << argv[1] <<  " " << shift <<" via " << filename << endl;
     cout << "Communication chiffrée avec César " << argv[1] <<  " " << shift <<" via " << filename << endl;
+    cout << "Votre message: ";
+    getline(cin, message);  //Entrée du message
+    sens = rightShift;
+    send(message, sens, shift);
     return 0;
 }
+
